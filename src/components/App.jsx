23529c770c6cf22d43d509f6bf725f6d45195cfe
roman Andrukhiv <andrukhivroman@gmail.com>
2023-04-11 22:02:47 +0200

    statistics component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
 import userData from 'user.json';
+import statsData from 'dataStat.json';
 
 export const App = () => {
   return (
     <div
       style={{
+        padding: '200px',
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
@@ -20,6 +24,7 @@ export const App = () => {
         avatar={userData.avatar}
         stats={userData.stats}
       />
+      <Statistics title="Upload stats" stats={statsData} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+import { randomColor } from '../../utils/randomColor';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
+
+      <ul className={css.stat_list}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            key={id}
+            className={css.stats_item}
+            style={{ backgroundColor: `#${randomColor()}` }}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ),
+};
